70142d95dac5c7a6f6f6329a605ca4a4a401db74
HenryP8 <91168994+HenryP8@users.noreply.github.com>
2021-09-29 10:22:13 -0400

    Update App.js

--- project_3/frontend/src/App.js
+++ project_3/frontend/src/App.js
@@ -7,7 +7,10 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       info: [],
-    }
+      value: "test",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.sendInfo = this.sendInfo.bind(this);
   }
 
   componentDidMount() {
@@ -21,8 +24,8 @@ export default class App extends React.Component {
     }).catch((err) => console.log(err))
   }
 
-  sendInfo(event) {
-    axios.post('/api/users/', {name : event.target.value, email : event.target.value + "@email.com"})
+  sendInfo() {
+    axios.post('/api/users/', {name : this.state.value, email : this.state.value + "@email.com"})
     .then(res => this.getData())
     .catch((err) => console.log(err))
   }
@@ -30,6 +33,10 @@ export default class App extends React.Component {
   handleDelete = (item) => {
     axios.delete(`/api/users/${item.id}/`)
     .then(res => this.getData())
+  }
+
+  handleChange(event) {
+    this.setState({ value : event.target.value });
   }
 
   render() {
@@ -43,10 +50,10 @@ export default class App extends React.Component {
           </li>
           ))}
       </ul>
-      <form>
+      <form onSubmit={this.sendInfo}>
         <label>
           Name:
-          <input type="text" onChange={this.sendInfo} />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
